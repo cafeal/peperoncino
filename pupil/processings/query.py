4fb22f8bb9abc5d0ab2f9175797ee414577be475
Junki Ishikawa <69guitar1015@gmail.com>
2019-11-10 21:58:56 +0900

    Implement Query

---
+++ pupil/processings/query.py
@@ -0,0 +1,16 @@
+from pupil import SeparatedProcessing
+
+class Query(SeparatedProcessing):
+    """Query records in dataframe.
+    
+    Parameters
+    ----------
+    query : str
+        query strings to be passed to pd.DataFrame.query.
+    """
+    def __init__(self, query: str):
+        super().__init__(is_fixed_rows=False)
+        self._query = query
+
+    def sep_process(self, df):
+        return df.query(self._query)
---
+++ tests/processings/test_query.py
@@ -0,0 +1,15 @@
+import pytest
+import pandas as pd
+import pupil as pp
+
+
+class TestQuery:
+    @pytest.mark.parametrize("query", ['a == 1', pytest.param('x == 1', marks=pytest.mark.xfail)])
+    def test_process(self, query):
+        df = pd.DataFrame({"a": [1, 2, 3], "b": [4, 5, 6]})
+
+        proc = pp.Query(query)
+        (df,) = proc.process([df])
+
+        assert df.equals(pd.DataFrame({"a": [1], "b": [4]}))
+
