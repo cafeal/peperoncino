d83002b41646436cd504a8d36f5be487f7bc4aa1
Junki Ishikawa <69guitar1015@gmail.com>
2020-01-12 22:27:53 +0900

    Update as_type.py

--- peperoncino/processings/as_type.py
+++ peperoncino/processings/as_type.py
@@ -14,8 +14,22 @@ class AsType(SeparatedProcessing):
 
     def __init__(self, mapping: Dict[str, str]):
         super().__init__()
-        self._mapping = mapping
+
+        _mapping = mapping.copy()
+        _dt_cols = []
+        for k in mapping:
+            if mapping[k] == "datetime":
+                _dt_cols.append(k)
+                _mapping.pop(k)
+            
+        self._mapping = _mapping
+        self._dt_cols = _dt_cols
 
     def sep_process(self, df: pd.DataFrame) -> pd.DataFrame:
         df = df.astype(self._mapping)
+
+        df = df.assign(**{
+            k: pd.to_datetime(df[k])
+            for k in self._dt_cols
+        })
         return df
--- tests/processings/test_as_type.py
+++ tests/processings/test_as_type.py
@@ -15,3 +15,23 @@ class TestAsType:
         xdf = df.copy()
         xdf.a = xdf.a.astype(dtype)
         assert_frame_equal(df, xdf)
+
+    def test_processing_datetime(self):
+        df = pd.DataFrame(
+            {
+                "a": [1, 2, 3],
+                "b": [
+                    "2020-01-01 00:00:00",
+                    "2020-01-01 00:01:00",
+                    "2020-01-02 00:01:00",
+                ],
+            }
+        )
+
+        proc = pp.AsType({"a": "int64", "b": "datetime"})
+        (df,) = proc.process([df])
+
+        xdf = df.copy()
+        xdf.a = xdf.a.astype("int64")
+        xdf.b = pd.to_datetime(xdf.b)
+        assert_frame_equal(df, xdf)
